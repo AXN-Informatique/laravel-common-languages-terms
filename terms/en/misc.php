8c85d44db8db5603d261cd693f8a4cc047f5f473
Vincent Garnier <forxer@gmail.com>
2016-11-22 11:45:37 +0100

    add "Back to top"

--- terms/en/misc.php
+++ terms/en/misc.php
@@ -3,6 +3,7 @@
 return [
 
     'back' => 'Back',
+    'back_to_top' => 'Back to top',
     'back_to_list' => 'Back to the list',
 
 ];
--- terms/fr/misc.php
+++ terms/fr/misc.php
@@ -3,6 +3,7 @@
 return [
 
     'back' => 'Retour',
+    'back_to_top' => 'Retour en haut',
     'back_to_list' => 'Retour à la liste',
 
 ];
