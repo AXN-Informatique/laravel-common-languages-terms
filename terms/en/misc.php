0ca82bc3c7208b686b4f31f184ee8d7590ed615a
Vincent Garnier <forxer@gmail.com>
2016-12-20 14:51:06 +0100

    add some misc terms

--- terms/en/misc.php
+++ terms/en/misc.php
@@ -2,6 +2,12 @@
 
 return [
 
+    'yes' => 'yes',
+    'no' => 'no',
+
+    'required' => 'required',
+    'required_notice' => 'Fields marked with :mark are mandatory.',
+
     'back' => 'Back',
     'back_home' => 'Back to home',
     'back_to_top' => 'Back to top',
--- terms/fr/misc.php
+++ terms/fr/misc.php
@@ -2,6 +2,12 @@
 
 return [
 
+    'yes' => 'oui',
+    'no' => 'non',
+
+    'required' => 'requis',
+    'required_notice' => 'Les champs marqués par :mark sont obligatoires.',
+
     'back' => 'Retour',
     'back_home' => 'Retour à l’accueil',
     'back_to_top' => 'Retour en haut',
