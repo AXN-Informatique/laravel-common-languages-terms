d5eb3ec2be00a0d97b54af8758d257dbc25739ae
Vincent Garnier <forxer@gmail.com>
2017-01-05 11:30:20 +0100

    add "recycle bin" term

--- terms/en/misc.php
+++ terms/en/misc.php
@@ -5,6 +5,8 @@ return [
     'yes' => 'yes',
     'no' => 'no',
 
+    'recycle_bin' => 'Recycle bin',
+
     'required' => 'required',
     'info_required_fields' => 'Fields marked with :mark are mandatory.',
 
--- terms/fr/misc.php
+++ terms/fr/misc.php
@@ -5,6 +5,8 @@ return [
     'yes' => 'oui',
     'no' => 'non',
 
+    'recycle_bin' => 'Corbeille',
+
     'required' => 'requis',
     'info_required_fields' => 'Les champs marqués par :mark sont obligatoires.',
 
