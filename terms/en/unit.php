d58d20835d3941388e29c2a86b36f16492b2efa3
Vincent Garnier <forxer@gmail.com>
2017-12-29 18:46:50 +0100

    add first units terms

---
+++ terms/en/unit.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'b' => 'b',
+    'bit' => 'bit',
+    'bits' => 'bits',
+
+    'B' => 'B',
+    'byte' => 'byte',
+    'bytes' => 'bytes',
+
+    'kB' => 'kB',
+    'MB' => 'MB',
+    'GB' => 'GB',
+    'TB' => 'TB',
+];
---
+++ terms/fr/unit.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'b' => 'b',
+    'bit' => 'bit',
+    'bits' => 'bits',
+
+    'B' => 'o',
+    'byte' => 'octet',
+    'bytes' => 'octets',
+
+    'kB' => 'ko',
+    'MB' => 'Mo',
+    'GB' => 'Go',
+    'TB' => 'To',
+];
