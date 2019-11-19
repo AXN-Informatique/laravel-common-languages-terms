3d0977d0412a5356284074ecfb5d6bfdc2e66568
Vincent Garnier <forxer@gmail.com>
2019-11-19 22:26:45 +0100

    add pagination previous/next terms

---
+++ terms/en/pagination.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'previous' => 'Previous',
+    'previous_fem' => 'Previous',
+
+    'next' => 'Next',
+    'next_fem' => 'Next',
+
+];
---
+++ terms/fr/pagination.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'previous' => 'Précédent',
+    'previous_fem' => 'Précédente',
+
+    'next' => 'Suivant',
+    'next_fem' => 'Suivante',
+
+];
